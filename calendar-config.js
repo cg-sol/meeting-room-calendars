1c7a66257927a41498ffcf8c4ed4503d96ca4a90
cg-sol <solect-cg@coffergroup.com>
2026-02-08 07:47:40 -0800

    Update calendar-config.js

--- calendar-config.js
+++ calendar-config.js
@@ -1,92 +1,95 @@
+
+
 const CALENDARS = [
     {
         name: "Room 1",
-        url: "http://outlook.office365.com/calendar/published/d1223f3c7c3d478ea3edf6435b0f48b5@solect.com/965d89338a714c34802d6a7b45bbff451948969580000036295/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/d1223f3c7c3d478ea3edf6435b0f48b5@solect.com/965d89338a714c34802d6a7b45bbff451948969580000036295/calendar.ics",
         color: "#0078d4"
     },
     {
         name: "Room 2",
-        url: "http://outlook.office365.com/owa/calendar/024f54540dad4a1fb19faa38e4efb1b6@solect.com/abd538cb08c6422784f06b70ecf8f3f81892744134491062028/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/024f54540dad4a1fb19faa38e4efb1b6@solect.com/abd538cb08c6422784f06b70ecf8f3f81892744134491062028/calendar.ics",
         color: "#107c10"
     },
     {
         name: "Room 3",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
         color: "#d83b01"
     },
     {
         name: "Room 4",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_4_ID@solect.com/REPLACE_WITH_ROOM_4_KEY/calendar.ics",
         color: "#8764b8"
     },
     {
         name: "Room 5",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_5_ID@solect.com/REPLACE_WITH_ROOM_5_KEY/calendar.ics",
         color: "#ea4300"
     },
     {
         name: "Room 6",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_6_ID@solect.com/REPLACE_WITH_ROOM_6_KEY/calendar.ics",
         color: "#00188f"
     },
     {
         name: "Room 7",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_7_ID@solect.com/REPLACE_WITH_ROOM_7_KEY/calendar.ics",
         color: "#008272"
     },
     {
         name: "Room 8",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_8_ID@solect.com/REPLACE_WITH_ROOM_8_KEY/calendar.ics",
         color: "#b146c2"
     },
     {
         name: "Room 9",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_9_ID@solect.com/REPLACE_WITH_ROOM_9_KEY/calendar.ics",
         color: "#00b294"
     },
     {
         name: "Room 10",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_10_ID@solect.com/REPLACE_WITH_ROOM_10_KEY/calendar.ics",
         color: "#ff8c00"
     },
     {
         name: "Room 11",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_11_ID@solect.com/REPLACE_WITH_ROOM_11_KEY/calendar.ics",
         color: "#6b69d6"
     },
     {
         name: "Room 12",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_12_ID@solect.com/REPLACE_WITH_ROOM_12_KEY/calendar.ics",
         color: "#00cc6a"
     },
     {
         name: "Room 13",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_13_ID@solect.com/REPLACE_WITH_ROOM_13_KEY/calendar.ics",
         color: "#e3008c"
     },
     {
         name: "Room 14",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_14_ID@solect.com/REPLACE_WITH_ROOM_14_KEY/calendar.ics",
         color: "#ffaa44"
     },
     {
         name: "Room 15",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_15_ID@solect.com/REPLACE_WITH_ROOM_15_KEY/calendar.ics",
         color: "#567c73"
     },
     {
         name: "Room 16",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_ROOM_16_ID@solect.com/REPLACE_WITH_ROOM_16_KEY/calendar.ics",
         color: "#8e8cd8"
     },
     {
         name: "Board Room",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_BOARDROOM_ID@solect.com/REPLACE_WITH_BOARDROOM_KEY/calendar.ics",
         color: "#da3b01"
     },
     {
         name: "South Street Conference Room",
-        url: "http://outlook.office365.com/owa/calendar/40350fe13e244f609ded65b4148ded8c@solect.com/53bd571d74f340dbaa0f761c8b665f7812053080795729831175/calendar.ics",
+        url: "https://outlook.office365.com/calendar/published/REPLACE_WITH_SOUTHST_ID@solect.com/REPLACE_WITH_SOUTHST_KEY/calendar.ics",
         color: "#0063b1"
     }
 ];
+
